3268c19a5de04c5c3115f80b663989e30c5bd36f
lmammino <lmammino@oryzone.com>
2012-07-21 13:02:46 +0200

    travis

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,12 +2,12 @@
 
 $vendorDir = __DIR__ . '/../vendor';
 
-if (!@include($vendorDir . '/.composer/autoload.php')) {
+/*if (!@include($vendorDir . '/.composer/autoload.php')) {
     die("You must set up the project dependencies, run the following commands:
 wget http://getcomposer.org/composer.phar
 php composer.phar install
 ");
-}
+}*/
 
 spl_autoload_register(function($class) {
     if (0 === (strpos($class, 'Oryzone\\Bundle\\ImageValidatorBundle\\'))) {
